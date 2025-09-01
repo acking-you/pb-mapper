c7cc7c9cf6e350229f19256db3c8b77b2b82aafb
LB7666 <acking-you@foxmail.com>
2025-09-02 03:54:27 +0800

    add network perms

--- ui/android/app/build.gradle.kts
+++ ui/android/app/build.gradle.kts
@@ -28,6 +28,13 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        
+        // Optimize APK size by only including essential Android architectures
+        ndk {
+            abiFilters += listOf("arm64-v8a", "armeabi-v7a")
+            // Removed "x86_64" to reduce APK size - only include ARM architectures
+            // Most Android devices use ARM processors
+        }
     }
 
     buildTypes {
